9ea3f51ecf16bf3f297fc981f7e21a4226db2d02
EiiiDOo <ahmed.eid159753@gmail.com>
2024-09-27 11:42:05 +0300

    make function of get fav and gome data emit to mutablestateflow

--- app/src/main/java/com/example/weatherapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/weatherapp/ui/main/MainViewModel.kt
@@ -4,39 +4,76 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.weatherapp.StateGeneric
+import com.example.weatherapp.model.CustomSaved
 import com.example.weatherapp.model.WeatherData
 import com.example.weatherapp.model.WeatherForecastFiveDays
 import com.example.weatherapp.repo.IRepo
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
-class MainViewModel(private val _repo : IRepo) : ViewModel() {
-    private val _responseWeatherOfLocationByLongAndlat: MutableStateFlow<StateGeneric<WeatherData?>> = MutableStateFlow(StateGeneric.Loading)
+class MainViewModel(private val _repo: IRepo) : ViewModel() {
+    private val _responseWeatherOfLocationByLongAndlat: MutableStateFlow<StateGeneric<WeatherData?>> =
+        MutableStateFlow(StateGeneric.Loading)
     val responseOfWeather = _responseWeatherOfLocationByLongAndlat
-   private val _responseForecastOfLocationByLongAndlat: MutableStateFlow<StateGeneric<WeatherForecastFiveDays?>> = MutableStateFlow(StateGeneric.Loading)
+
+    private val _responseForecastOfLocationByLongAndlat: MutableStateFlow<StateGeneric<WeatherForecastFiveDays?>> =
+        MutableStateFlow(StateGeneric.Loading)
     val responseOfForecast = _responseForecastOfLocationByLongAndlat
 
-    fun getWeatherByLongitudeAndLatitude(lat:Double,lon:Double){
+    private val _allFavourites: MutableStateFlow<StateGeneric<List<CustomSaved>>> =
+        MutableStateFlow(StateGeneric.Loading)
+    val allFavourite = _allFavourites
+
+    private val _homeData: MutableStateFlow<StateGeneric<CustomSaved>> =
+        MutableStateFlow(StateGeneric.Loading)
+    val homeData = _homeData
+
+    fun getWeatherByLongitudeAndLatitude(lat: Double, lon: Double) {
 
         viewModelScope.launch {
-            val res = _repo.getWeatherForLocation(lat,lon)
+            val res = _repo.getWeatherForLocation(lat, lon)
             if (res.isSuccessful) {
                 _responseWeatherOfLocationByLongAndlat.value = StateGeneric.Success(res.body())
-            }else{
+            } else {
                 _responseWeatherOfLocationByLongAndlat.value = StateGeneric.Error(res.message())
             }
         }
     }
-    fun getForecastByLongitudeAndLatitude(lat:Double,lon:Double){
+
+    fun getForecastByLongitudeAndLatitude(lat: Double, lon: Double) {
 
         viewModelScope.launch {
-            val res = _repo.getWeatherEveryThreeHours(lat,lon)
+            val res = _repo.getWeatherEveryThreeHours(lat, lon)
             if (res.isSuccessful) {
                 _responseForecastOfLocationByLongAndlat.value = StateGeneric.Success(res.body())
-                Log.d("TAG", "getForecastByLongitudeAndLatitude: "+res.body().toString())
-            }else{
+                Log.d("TAG", "getForecastByLongitudeAndLatitude: " + res.body().toString())
+            } else {
                 _responseForecastOfLocationByLongAndlat.value = StateGeneric.Error(res.message())
             }
         }
     }
+
+    suspend fun saveWeatherData(weatherData: CustomSaved): Long = _repo.insert(weatherData)
+
+    suspend fun deleteWeatherData(weatherData: CustomSaved): Int = _repo.delete(weatherData)
+
+    fun getHomeWeatherData() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _repo.getHomeWeatherData().collect {
+                _homeData.value = StateGeneric.Success(it)
+            }
+        }
+    }
+
+    fun getFavouriteWeatherData() {
+        viewModelScope.launch(Dispatchers.IO) {
+            //.shareIn(this, started = SharingStarted.Lazily)
+            _repo.getFavWeatherData().collect {
+                _allFavourites.value = StateGeneric.Success(it)
+            }
+        }
+
+    }
+
 }
