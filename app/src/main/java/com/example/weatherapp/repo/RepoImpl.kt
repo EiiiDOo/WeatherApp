6de6b257ee0f80117a55b5b4533c6448f4c789fb
EiiiDOo <ahmed.eid159753@gmail.com>
2024-09-27 11:52:24 +0300

    add functions to local in repo impl

--- app/src/main/java/com/example/weatherapp/repo/RepoImpl.kt
+++ app/src/main/java/com/example/weatherapp/repo/RepoImpl.kt
@@ -1,14 +1,17 @@
 package com.example.weatherapp.repo
 
+import com.example.weatherapp.local.ILocalDataSource
+import com.example.weatherapp.model.CustomSaved
 import com.example.weatherapp.model.WeatherData
 import com.example.weatherapp.model.WeatherForecastFiveDays
 import com.example.weatherapp.remote.IRemotedataSource
+import kotlinx.coroutines.flow.Flow
 import retrofit2.Response
 
-class RepoImpl private constructor(val remote: IRemotedataSource) : IRepo {
+class RepoImpl private constructor(val remote: IRemotedataSource,val local: ILocalDataSource) : IRepo {
     companion object{
-        fun getInstance(remote: IRemotedataSource): IRepo {
-            return RepoImpl(remote)
+        fun getInstance(remote: IRemotedataSource,local: ILocalDataSource): IRepo {
+            return RepoImpl(remote,local)
         }
     }
     override suspend fun getWeatherByCountryName(q: String, appid: String): Response<WeatherData> {
@@ -35,4 +38,11 @@ class RepoImpl private constructor(val remote: IRemotedataSource) : IRepo {
         return remote.getWeatherEveryThreeHours(lat, lon, lang, units, appid)
     }
 
+    override fun getFavWeatherData(): Flow<List<CustomSaved>> =local.getFavWeatherData()
+
+    override fun getHomeWeatherData(): Flow<CustomSaved> =local.getHomeWeatherData()
+
+    override suspend fun insert(customSaved: CustomSaved) : Long =local.insert(customSaved)
+
+    override suspend fun delete(customSaved: CustomSaved): Int = local.delete(customSaved)
 }
