aa2544c7825d612cbc26d2aa88ff719dc8bc6f12
EiiiDOo <ahmed.eid159753@gmail.com>
2024-09-27 11:52:53 +0300

    show appbar in fragment

--- app/src/main/java/com/example/weatherapp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/settings/SettingsFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.weatherapp.R
 import com.example.weatherapp.databinding.FragmentSettingsBinding
+import com.example.weatherapp.ui.main.MainActivity
 
 
 class SettingsFragment : Fragment() {
@@ -24,6 +25,17 @@ class SettingsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as MainActivity).showAppBar(true)
+
     }
 
 }
